b845cf46ad187d9cd6a9a46b9ace468ee6493fbe
James Pike <jamesandrewpike@gmail.com>
2024-11-01 08:50:01 -0400

    commit

--- src/components/widgets/Hero.tsx
+++ src/components/widgets/Hero.tsx
@@ -24,7 +24,7 @@ export default component$(() => {
                   Comprehensive Legal Support
                 </span>{" "}
                 specializing in Divorce, Child Custody, Spousal Support,
-                Property Division, Adoption, Mediation and more. Schedule a <em>Free Consultation</em> today to discuss how we can support your legal needs and help you
+                Property Division, Adoption, Mediation and more. Schedule a <em class="text-sky-500">Free Consultation</em> today to discuss how we can support your legal needs and help you
                 navigate the complexities of family law.
               </p>
             
